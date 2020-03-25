4738df0662930463997dae111468a73b4f37bb8d
KyiPhyu <kyiphyu@eramyanmar.com>
2020-03-25 10:36:12 +0630

    Add method

--- app/Member.php
+++ app/Member.php
@@ -1,14 +1,49 @@
 <?php
 
-use Illuminate\Http\Request;
+namespace App;
 
-class Member {
-    public function one(Request $request) 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class Member extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+
+    Public function age() 
     {
-        
+        return 32;
     }
 
-    protected function two()
+    public function foobar()
     {
         
     }
