96b842f0626bddc7fb1a7304d23a3ad985e26bd8
KyiPhyu <kyiphyu@eramyanmar.com>
2020-03-25 10:28:03 +0630

    Install freamework

---
+++ app/Member.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Http\Request;
+
+class Member {
+    public function one(Request $request) 
+    {
+        
+    }
+
+    protected function two()
+    {
+        
+    }
+}
